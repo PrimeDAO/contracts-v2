c0e6d042ffffbf33713dd80e03b62b0461a985a0
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-06-04 19:48:39 +0530

    updated deployFromModule

--- contracts/seed/SeedFactory.sol
+++ contracts/seed/SeedFactory.sol
@@ -132,9 +132,50 @@ contract SeedFactory is CloneFactory, Ownable {
 
     /**
       * @dev                          Deploys Seed contract from safe without any confirmation.
+      * @param _admin                 The address of the admin of this contract. Funds contract
+                                      and has permissions to whitelist users, pause and close contract.
+      * @param _tokens                Array containing two params:
+                                        - The address of the seed token being distributed.
+      *                                 - The address of the funding token being exchanged for seed token.
+      * @param _softHardThresholds     Array containing two params:
+                                        - the minimum funding token collection threshold in wei denomination.
+                                        - the highest possible funding token amount to be raised in wei denomination.
+      * @param _price                 The price in wei of fundingTokens when exchanged for seedTokens.
+      * @param _startTime             Distribution start time in unix timecode.
+      * @param _endTime               Distribution end time in unix timecode.
+      * @param _vestingDuration       Vesting period duration in days.
+      * @param _vestingCliff          Cliff duration in days.
+      * @param _isWhitelisted         Set to true if only whitelisted adresses are allowed to participate.
+      * @param _fee                   Success fee expressed in Wei as a % (e.g. 2 = 2% fee)
+      * @param _metadata              Seed contract metadata, that is IPFS URI
     */
-    function deployFromModule() public {
-        bytes memory data = abi.encodeWithSignature("deploySeed()");
+    function deployFromModule(
+        address _admin,
+        address[] memory _tokens,
+        uint256[] memory _softHardThresholds,
+        uint256 _price,
+        uint256 _startTime,
+        uint256 _endTime,
+        uint32 _vestingDuration,
+        uint32 _vestingCliff,
+        bool _isWhitelisted,
+        uint8 _fee,
+        bytes32 _metadata
+    ) public {
+        bytes memory data = abi.encodeWithSignature(
+            "deploySeed()", 
+            address(safe), 
+            _admin, 
+            _tokens,
+            _softHardThresholds, 
+            _price, 
+            _startTime, 
+            _endTime, 
+            _vestingDuration, 
+            _vestingCliff,
+            _isWhitelisted,
+            _fee,
+            _metadata);
         require(safe.execTransactionFromModule(address(this), 0, data, Enum.Operation.Call), "Failed");
     }
 
