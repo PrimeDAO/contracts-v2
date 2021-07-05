b48bcd71d35d7dbfbd9d939f80b709663af69306
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-07-05 13:42:41 +0200

    adds spec for double claim revert

--- test/prime_v2_token/MerkleDrop.spec.js
+++ test/prime_v2_token/MerkleDrop.spec.js
@@ -1,13 +1,6 @@
 const { expect } = require("chai");
 const { utils } = require("ethers");
-const BN = require("bn.js");
-const { setupFixture, parsedAllocations } = require("./utils/setupHelpers");
-const {
-  parseToBnArr,
-  createTreeWithAccounts,
-  getTranche,
-  getAccountBalanceProof,
-} = require("./merkle");
+const { setupFixture } = require("./utils/setupHelpers");
 const { ethers } = require("hardhat");
 
 const commonState = {
@@ -23,61 +16,83 @@ describe(">> MerkleDrop", () => {
     alice = signers[2];
   });
 
-  describe("$ thresholdBlock lies in the future", () => {
-    const initialState = {
-      ...commonState,
-      thresholdInPast: false,
-      withProof: true,
-    };
+  describe("# claimTranche", () => {
+    describe("$ thresholdBlock lies in the future", () => {
+      const initialState = {
+        ...commonState,
+        thresholdInPast: false,
+        withProof: true,
+      };
 
-    beforeEach(async () => {
-      ({
-        merkleDropInstance,
-        v2TokenInstance,
-        tree,
-        proof,
-        trancheIdx,
-        expectedBalance,
-      } = await setupFixture({
-        initialState,
-      }));
+      beforeEach(async () => {
+        ({
+          merkleDropInstance,
+          v2TokenInstance,
+          tree,
+          proof,
+          trancheIdx,
+          expectedBalance,
+        } = await setupFixture({
+          initialState,
+        }));
+      });
+
+      it("reverts", async () => {
+        const claim = merkleDropInstance
+          .connect(alice)
+          .claimTranche(alice.address, trancheIdx, expectedBalance, proof);
+        expect(claim).to.be.revertedWith("Rewards are not yet claimable");
+      });
     });
 
-    it("reverts", async () => {
-      const claim = merkleDropInstance
-        .connect(alice)
-        .claimTranche(alice.address, trancheIdx, expectedBalance, proof);
-      expect(claim).to.be.revertedWith("Rewards are not yet claimable");
+    describe("$ thresholdBlock lies in the past", () => {
+      const initialState = {
+        ...commonState,
+        thresholdInPast: true,
+        withProof: true,
+      };
+
+      beforeEach(async () => {
+        ({
+          merkleDropInstance,
+          v2TokenInstance,
+          tree,
+          proof,
+          trancheIdx,
+          expectedBalance,
+        } = await setupFixture({
+          initialState,
+        }));
+
+        await merkleDropInstance
+          .connect(alice)
+          .claimTranche(alice.address, trancheIdx, expectedBalance, proof);
+      });
+
+      it("lets alice claim her allocation", async () => {
+        const aliceBalance = await v2TokenInstance.balanceOf(alice.address);
+        expect(aliceBalance).to.eq(expectedBalance);
+      });
+
+      it("reverts if alice claims a second time", async () => {
+        const duplicateClaim = merkleDropInstance.claimTranche(
+          alice.address,
+          trancheIdx,
+          expectedBalance,
+          proof
+        );
+        expect(duplicateClaim).to.be.revertedWith("LP has already claimed");
+      });
     });
   });
 
-  describe("$ thresholdBlock lies in the past", () => {
-    const initialState = {
-      ...commonState,
-      thresholdInPast: true,
-      withProof: true,
-    };
-
-    beforeEach(async () => {
-      ({
-        merkleDropInstance,
-        v2TokenInstance,
-        tree,
-        proof,
-        trancheIdx,
-        expectedBalance,
-      } = await setupFixture({
-        initialState,
-      }));
-
-      await merkleDropInstance
-        .connect(alice)
-        .claimTranche(alice.address, trancheIdx, expectedBalance, proof);
-    });
-
-    it("lets alice claim her allocation", async () => {
-      const aliceBalance = await v2TokenInstance.balanceOf(alice.address);
-      expect(aliceBalance).to.eq(expectedBalance);
+  describe("# verifyClaim", () => {
+    describe("$ claim is valid", () => {
+      const initialState = {
+        ...commonState,
+        thresholdInPast: true,
+        withProof: true,
+      };
     });
   });
 });
