e1171c09dbd9ec987ddaa280faefbd7cd5cbae44
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-07-05 16:40:15 +0200

    adds deploy scripts

--- contracts/prime_token_v2/PrimeTokenV2.sol
+++ contracts/prime_token_v2/PrimeTokenV2.sol
@@ -16,13 +16,12 @@ import "openzeppelin-contracts-sol8/token/ERC20/extensions/ERC20Capped.sol";
 contract PrimeTokenV2 is ERC20Capped {
 
     constructor(
-    	uint256 initialSupply,
     	address genesisMultisig
     )
     ERC20("PrimeDAO Token V2", "PRIMEv2")
     ERC20Capped(100000000*10**18)
     {
-        ERC20._mint(genesisMultisig, initialSupply);
+        ERC20._mint(genesisMultisig, 100000000*10**18);
     }
 
 }
