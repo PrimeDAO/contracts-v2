d821b370a11f2a6d13a91686ac168a71cb6c6e25
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-07-18 18:46:36 +0530

    fixed merge conflict at gnosis.js

--- scripts/utils/gnosis.js
+++ scripts/utils/gnosis.js
@@ -1,48 +1,5 @@
 const axios = require('axios');
 
-<<<<<<< HEAD
-/* eslint-disable */
-const getUrl = (network) => {
-    switch(network){
-        case 'mainnet': return `https://safe-transaction.gnosis.io/api/v1/safes/`;
-        case 'rinkeby': return `https://safe-transaction.rinkeby.gnosis.io/api/v1/safes/`;
-        default: return `${network}, is not supported yet`;
-    }
-}
-
-const getRelayUrl = (network) => {
-    switch(network) {
-        case 'mainnet': return `https://safe-relay.gnosis.io/api/v2/safes/`;
-        case 'rinkeby': return `https://safe-relay.rinkeby.gnosis.io/api/v2/safes/`;
-        default:  `${network}, is not supported yet`;
-    }
-}
-
-const post = async (method, payload, safe, url) => {
-    console.log("Post",url);
-    const res = await axios.post(
-        `${url}${safe}${methods[method]}`,
-        payload
-    );
-    return res;
-}
-
-const get = async (method, safe, url) => {
-    console.log("Get",url);
-    const res = await axios.get(
-        `${url}${safe}${methods[method]}`
-    );
-    return res.data;
-}
-
-const getEstimate = async (payload, safe, url) => {
-    console.log(url);
-    const res = await axios.post(
-        `${url}${safe}/transactions/estimate/`,
-        payload
-    );
-    return res;
-=======
 const parseErrorData = (data) => {
     return Object.values(data).reduce(
         (accumulator, error) => `${accumulator}${accumulator.length == 0?'':' , '}${error}`, ''
@@ -99,7 +56,6 @@ const get = async (method, safe, url) => {
     } catch (error) {
         throw Error(errorHandler(error).message);
     }
->>>>>>> main
 }
 
 const getCurrentNonce = async (safe, url) => {
@@ -107,11 +63,8 @@ const getCurrentNonce = async (safe, url) => {
     return transactions.countUniqueNonce;
 }
 
-<<<<<<< HEAD
-=======
 const getEstimate = async (payload, safe, url) => await post('getEstimate', payload, safe, url);
 
->>>>>>> main
 const sendTransaction = async (payload, safe, url) => await post('sendTransaction', payload, safe, url);
 const getTransactionHistory = async (safe, url) => await get('getTransactionHistory', safe, url);
 
@@ -120,18 +73,10 @@ const getDelegates = async (safe, url) => (await get('getDelegates', safe, url))
 
 const api = (safe, network) => {
     const url = getUrl(network);
-<<<<<<< HEAD
-    const relayUrl = getRelayUrl(network);
-    return {
-        sendTransaction: async (payload) => await sendTransaction(payload, safe, url),
-        addDelegate: async (payload) => await await addDelegate(payload, safe, url),
-        getEstimate: async (payload) => await getEstimate(payload, safe, relayUrl),
-=======
     return {
         sendTransaction: async (payload) => await sendTransaction(payload, safe, url),
         addDelegate: async (payload) => await await addDelegate(payload, safe, url),
         getEstimate: async (payload) => await getEstimate(payload, safe, url),
->>>>>>> main
         getTransactionHistory: async () => await getTransactionHistory(safe, url),
         getCurrentNonce: async () => await getCurrentNonce(safe, url),
         getDelegates: async () => await getDelegates(safe, url)
