df757ef7879464300526de1bc9c012d8a2f9a5a3
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-07-20 10:26:13 +0200

    adds task for addDelegate

---
+++ tasks/seedManagement.js
@@ -0,0 +1,10 @@
+const { task } = require("hardhat/config");
+
+task("changeOwner", "changes owner of SeedFactory")
+  .addParam("address", "new owner address", undefined)
+  .setAction(async ({ address }, { ethers }) => {
+    console.log(`changing owner of SeedFactory to ${address}`);
+    const seedFactoryInstance = await ethers.getContract("SeedFactory");
+    const tx = await seedFactoryInstance.transferOwnership(address);
+    console.log("Transaction:", tx.hash);
+  });
