741e48d7c358d9846d725fe41e65cb80ba0f2fc8
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-07-26 13:16:45 +0200

    removes OZ dependency

--- contracts/test/ERC20Mock.sol
+++ contracts/test/ERC20Mock.sol
@@ -1,7 +1,7 @@
 // solium-disable linebreak-style
 pragma solidity 0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts-sol8/token/ERC20/ERC20.sol";
 
 
 contract ERC20Mock is ERC20 {
