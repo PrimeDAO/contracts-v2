15566548e7a6c7d8d47d357d760f7e89eb6bbb56
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-08-02 20:18:27 +0530

    added test for deployment and funding

--- test/unit/lbp-wrapper.js
+++ test/unit/lbp-wrapper.js
@@ -46,10 +46,12 @@ describe("Contract: LBPWrapper", async () => {
 		const END_WEIGHTS = [
 			parseEther('0.15').toString(), parseEther('0.85').toString()];
 		const UPDATE_DURATION = await time.duration.minutes(60);
+		let amounts = [parseEther('35').toString(), parseEther('65').toString()];
 		startTime = await now.add(await time.duration.minutes(10));
 		endTime = await startTime.add(UPDATE_DURATION);
 		swapEnabledOnStart = true;
 		nonce = 0;
+		let initUserData;
         before("!! deploy WrapperFactory", async () => {
             setup = await deploy();
 			tokenAddresses = [setup.tokenList[0].address, setup.tokenList[1].address];
@@ -60,6 +62,9 @@ describe("Contract: LBPWrapper", async () => {
 			setup.lbpWrapper = await (await ethers.getContractFactory("LBPWrapper", setup.roles.root)).deploy();
 			await setup.wrapperFactory.setMasterCopy(setup.lbpWrapper.address);
 			if(tokenAddresses[1]<tokenAddresses[0]){
+				const tokenContract = setup.tokenList[0];
+				setup.tokenList[0] = setup.tokenList[1];
+				setup.tokenList[1] = tokenContract;
 				const token = tokenAddresses[0];
 				tokenAddresses[0] = tokenAddresses[1];
 				tokenAddresses[1] = token;
@@ -69,7 +74,14 @@ describe("Contract: LBPWrapper", async () => {
 				weight = END_WEIGHTS[0];
 				END_WEIGHTS[0] = END_WEIGHTS[1];
 				END_WEIGHTS[1] = weight; 
+				amount = amounts[0];
+				amounts[0] = amounts[1];
+				amounts[1] = amount;
 			}
+			await setup.tokenList[0].connect(setup.roles.root).approve(setup.wrapperFactory.address, amounts[0]);
+			await setup.tokenList[1].connect(setup.roles.root).approve(setup.wrapperFactory.address, amounts[1]);
+			initUserData =ethers.utils.defaultAbiCoder.encode(['uint256', 'uint256[]'], 
+                                        [0, amounts]);
             expect(await setup.wrapperFactory.isInitialized()).to.equal(true);
 			expect(await setup.wrapperFactory.wrapperMasterCopy()).to.equal(setup.lbpWrapper.address);
         });
@@ -78,20 +90,25 @@ describe("Contract: LBPWrapper", async () => {
                 NAME,
 				SYMBOL,
 				tokenAddresses,
+				amounts,
 				START_WEIGHTS,
 				swapEnabledOnStart,
 				startTime.toNumber(),
 				endTime.toNumber(),
 				END_WEIGHTS,
-				setup.roles.prime.address
+				setup.roles.root.address,
+				initUserData
             )).wait();
 			const {lbp, wrapper} = receipt.events.filter((data) => {return data.event === 'LBPDeployedUsingWrapper'})[0].args;
 			setup.lbp = setup.Lbp.attach(lbp);
 			setup.lbpWrapper = setup.lbpWrapper.attach(wrapper);
 			expect(await setup.lbp.name()).to.equal(NAME);
 			expect(await setup.lbp.getOwner()).to.equal(wrapper);
-			expect(await setup.lbpWrapper.owner()).to.equal(setup.roles.prime.address);
+			expect(await setup.lbpWrapper.owner()).to.equal(setup.roles.root.address);
 			expect(await setup.lbp.symbol()).to.equal(SYMBOL);
+			const balances = (await setup.vault.getPoolTokens(setup.lbp.getPoolId())).balances;
+			expect(balances[0].toString()).to.equal(amounts[0]);
+			expect(balances[1].toString()).to.equal(amounts[1]);
         });
     });
 });
