9b94322cab4841629f31b8acc22338deb232206c
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-08-12 12:00:53 +0200

    fixes spec (await revertion)

--- test/unit/Reputation.spec.js
+++ test/unit/Reputation.spec.js
@@ -105,7 +105,7 @@ describe.only("Reputation", () => {
           newRepHolders,
           newRepAmounts
         );
-        expect(batchMintAttempt).to.be.revertedWith("fua");
+        await expect(batchMintAttempt).to.be.revertedWith("fua");
       });
     });
   });
