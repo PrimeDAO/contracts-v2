d535f8b433312fdc78dd8a33325434efbaf8772f
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-08-14 14:13:04 +0530

    removed unnecessary comments

--- contracts/utils/interface/ILBP.sol
+++ contracts/utils/interface/ILBP.sol
@@ -15,18 +15,12 @@
 pragma solidity ^0.8.0;
 
 interface ILBP {
-	
-	// function getGradualWeightUpdateParams() external view returns (
-	// 	uint256 startTime,
-	// 	uint256 endTime,
-	// 	uint256[] memory endWeights
-	// );
 
 	function updateWeightsGradually(
         uint256 startTime,
         uint256 endTime,
         uint256[] memory endWeights
-    ) external; // Had to remove the modifiers, was that correct?
+    ) external;
 
 	function getPoolId() external view returns (bytes32);
 }
