8c7b39c653cbf421196d999e47f0da3e59b0c4eb
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-08-17 13:08:38 +0200

    deployed to rinkeby

--- tasks/reputationManagement.js
+++ tasks/reputationManagement.js
@@ -1,14 +1,16 @@
+const { utils, BigNumber } = require("ethers");
 const { getReputationParams } = require("./utils/reputation");
 const initialRepBalances = require("../inputs/initialRepBalances.json");
 
+const { formatEther } = utils;
+
 task("verifyReputation", "Verifies the contract on etherscan").setAction(
   async (taskArgs, { run, ethers }) => {
-    const { repHolders, repAmounts } = getReputationParams(initialRepBalances);
     const reputationInstance = await ethers.getContract("Reputation");
 
     await run("verify:verify", {
       address: reputationInstance.address,
-      constructorArguments: [repHolders, repAmounts],
+      constructorArguments: [],
     });
   }
 );
