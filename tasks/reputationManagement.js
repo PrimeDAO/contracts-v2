0e92bef0891485378c36d24fdedddc096b25a5ee
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-08-17 11:41:52 +0200

    adapts verification task

--- tasks/reputationManagement.js
+++ tasks/reputationManagement.js
@@ -1,8 +1,9 @@
 const { getReputationParams } = require("./utils/reputation");
+const initialRepBalances = require("../inputs/initialRepBalances.json");
 
 task("verifyReputation", "Verifies the contract on etherscan").setAction(
   async (taskArgs, { run, ethers }) => {
-    const { repHolders, repAmounts } = getReputationParams();
+    const { repHolders, repAmounts } = getReputationParams(initialRepBalances);
     const reputationInstance = await ethers.getContract("Reputation");
 
     await run("verify:verify", {
