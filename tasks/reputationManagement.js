62964989664c2f308e31d48804b062ee07c6d2a3
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-08-17 13:01:19 +0200

    adds task to batchMint

--- tasks/reputationManagement.js
+++ tasks/reputationManagement.js
@@ -18,9 +18,29 @@ task(
   "Transfers ownership of Reputation to specified address"
 )
   .addParam("owner", "address of new owner", undefined, types.string)
-  .setAction(async ({ owner }, { run, ethers }) => {
+  .setAction(async ({ owner }, { ethers }) => {
     console.log(`Transferring ownership to ${owner}`);
     const reputationInstance = await ethers.getContract("Reputation");
     const tx = await reputationInstance.transferOwnership(owner);
     console.log("tx: ", tx.hash);
   });
+
+task("batchMint", "Mints initial REP balances").setAction(
+  async (taskArgs, { ethers }) => {
+    const { repHolders, repAmounts } = getReputationParams(initialRepBalances);
+    const batchMintAmount = formatEther(
+      repAmounts.reduce(
+        (accumulator, currentValue) => accumulator.add(currentValue),
+        BigNumber.from(0)
+      )
+    );
+
+    console.log(
+      `Batch-minting ${batchMintAmount} to ${repHolders.length} addresses`
+    );
+
+    const reputationInstance = await ethers.getContract("Reputation");
+    const tx = await reputationInstance.batchMint(repHolders, repAmounts);
+    console.log("tx: ", tx.hash);
+  }
+);
