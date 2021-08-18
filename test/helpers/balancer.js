2064701ac91358ace71a1e958db09927e2e1f0f7
Marvin <marvin88gr@gmail.com>
2021-08-18 15:43:07 +0200

    name changes plus implemented getContractFactory

--- test/helpers/balancer.js
+++ test/helpers/balancer.js
@@ -1,52 +1,40 @@
 const LBPArtifact = require("../../imports/LiquidityBootstrappingPool.json");
-const LBPFactoryArtifact = require('../../imports/LiquidityBootstrappingPoolFactory.json');
-const VaultArtifact = require("../../imports/Vault.json");
-const AuthorizerArtifact = require("../../imports/Authorizer.json");
-const { constants } = require('@openzeppelin/test-helpers');
 const { ethers } = require("hardhat");
 
-const LBPFactory = async (setup) => {
-    const LBPFactory_Factory = new ethers.ContractFactory(LBPFactoryArtifact.abi, LBPFactoryArtifact.bytecode, setup.roles.root);
-    const lbpFactory = await LBPFactory_Factory.deploy(setup.vault.address);
-    return lbpFactory;
+const getLbpFactoryInstance = async (setup) => {
+    const lbPFactoryFactory = await ethers.getContractFactory("LiquidityBootstrappingPoolFactory");
+    const lbpFactoryInstance = await lbPFactoryFactory.deploy(setup.vault.address);
+    return lbpFactoryInstance;
 }
 
-const Vault = async (setup) => {
+const getVaultInstance = async (setup) => {
 
     const pauseWindowDuration = 0;
     const bufferPeriodDuration = 0;
 
-    const WETH_Factory = await ethers.getContractFactory(
+    const WETHFactory = await ethers.getContractFactory(
 		"ERC20Mock",
-		setup.roles.root
 	);
-    const WETH = await WETH_Factory.deploy("WRAPETH", "WETH");
+    const WETHInstance = await WETHFactory.deploy("WRAPETH", "WETH");
     
-    const Authorizer_Factory = new ethers.ContractFactory(
-        AuthorizerArtifact.abi,
-        AuthorizerArtifact.bytecode,
-        setup.roles.root);
-        const authorizer = await Authorizer_Factory.deploy(setup.roles.root.address);
+    const authorizerFactory = await ethers.getContractFactory("Authorizer");
+    const authorizerInstance = await authorizerFactory.deploy(setup.roles.root.address);
         
-    const Vault_factory = new ethers.ContractFactory(
-        VaultArtifact.abi,
-        VaultArtifact.bytecode,
-        setup.roles.root);
-    const vault = await Vault_factory.deploy(
-        authorizer.address,
-        WETH.address,
+    const vaultFactory = await ethers.getContractFactory("Vault");
+    const vaultInstance = await vaultFactory.deploy(
+        authorizerInstance.address,
+        WETHInstance.address,
         pauseWindowDuration,
         bufferPeriodDuration);
 
 
-    return vault;
+    return vaultInstance;
 }
 
-const Lbp = (setup) => new ethers.ContractFactory(LBPArtifact.abi, LBPArtifact.bytecode, setup.roles.root);
-
+const getLbpFactory = (setup) => new ethers.ContractFactory(LBPArtifact.abi, LBPArtifact.bytecode, setup.roles.root);
 
 module.exports = {
-	LBPFactory,
-	Vault,
-	Lbp,
+	getLbpFactoryInstance,
+	getVaultInstance,
+	getLbpFactory,
 }
