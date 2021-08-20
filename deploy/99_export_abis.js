274f1189d8428870001f5880b6a4987437f6a58e
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-08-20 16:01:56 +0200

    adds export script to deploy

--- deploy/99_export_abis.js
+++ deploy/99_export_abis.js
@@ -1,17 +1,34 @@
 const fs = require("fs").promises;
 const path = require("path");
-const compressAbis = require("../helperscript");
 
 const networks = ["rinkeby", "mainnet"];
 
 const sharedAbiConfig = {
   PrimeToken: "ERC20",
+  Dai: "ERC20",
+  Weth: "ERC20",
 };
 
+const compressAbis = (abisObject, sharedAbiConfig) => {
+  const compressedAbiObject = { ...abisObject };
+  const { contracts } = compressedAbiObject;
+
+  for (const contractName in contracts) {
+    if (sharedAbiConfig[contractName]) {
+      contracts[contractName].abi = sharedAbiConfig[contractName];
+    }
+  }
+
+  return compressedAbiObject;
+};
+
+// this function runs after a new deployment if the tag "Export" is specified
+// it generates the abis and addresses for the frontend and compresses the resulting file
+// by saving just one abi representation for contracts that share the same ABI (e.g. ERC20)
 const exportAbiFunction = async ({ run, network }) => {
   const { name } = network;
 
-  // if (!networks.includes(name)) return;
+  if (!networks.includes(name)) return;
 
   const targetPath = path.resolve(__dirname, `../exports/${name}.json`);
   await run("export", { export: targetPath });
