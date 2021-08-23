71b120716cf2a3fb0107e9641a2ba64000a63296
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-08-23 10:09:23 +0200

    removes contractAddresses

--- deploy/00_deploy_seed_contracts.js
+++ deploy/00_deploy_seed_contracts.js
@@ -1,6 +1,4 @@
 const DeployedContracts = require("../contractAddresses.json");
-const path = require("path");
-const fs = require("fs");
 
 const deployFunction = async ({ getNamedAccounts, deployments, network }) => {
   const { deploy } = deployments;
@@ -10,34 +8,24 @@ const deployFunction = async ({ getNamedAccounts, deployments, network }) => {
   const { address: seedFactoryAddress } = await deploy("SeedFactory", {
     from: root,
     args: [],
-    log: true
+    log: true,
   });
 
   const { address: seedAddress } = await deploy("Seed", {
     from: root,
     args: [],
-    log: true
+    log: true,
   });
 
   const seedFactoryInstance = await ethers.getContract("SeedFactory");
 
   await seedFactoryInstance.setMasterCopy(seedAddress);
 
-  const { address: signerAddress } = await deploy("Signer", {
+  await deploy("Signer", {
     from: root,
     args: [Safe, seedFactoryAddress],
-    log: true
+    log: true,
   });
-
-  DeployedContracts[network.name].Signer = signerAddress;
-  DeployedContracts[network.name].SeedFactory = seedFactoryAddress;
-  DeployedContracts[network.name].Seed = seedAddress;
-
-  console.log("Saving Address to contractAddresses.json\n");
-  fs.writeFileSync(
-    path.resolve(__dirname, "../contractAddresses.json"),
-    JSON.stringify(DeployedContracts)
-  );
 };
 
 module.exports = deployFunction;
