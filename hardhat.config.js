6e4395f7ebd0facfa2cd33a227dc55f1383b2aa9
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-08-30 16:29:41 +0200

    adapts hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -46,6 +46,7 @@ module.exports = {
       blockGasLimit: 10000000000000,
       gas: 200000000000,
       saveDeployments: false,
+      initialBaseFeePerGas: 0,
       hardfork: "london",
     },
     mainnet: {
