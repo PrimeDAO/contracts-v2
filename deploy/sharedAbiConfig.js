4c7b8a618c09997fc8217dadf4b059492a9ba057
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-09-03 15:05:15 +0200

    tmp

--- deploy/sharedAbiConfig.js
+++ deploy/sharedAbiConfig.js
@@ -1,6 +1,7 @@
 const ownDeployedContracts = {
   PrimeToken: {
-    abi: "IERC20",
+    abi: "ERC20",
+    interfaceAbi: "IERC20",
   },
 };
 
@@ -8,21 +9,25 @@ const externalContracts = {
   rinkeby: {
     DAI: {
       address: "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa",
-      abi: "IERC20",
+      abi: "ERC20",
+      interfaceAbi: "IERC20",
     },
     WETH: {
       address: "0xc778417E063141139Fce010982780140Aa0cD5Ab",
-      abi: "IERC20",
+      abi: "ERC20",
+      interfaceAbi: "IERC20",
     },
   },
   mainnet: {
     DAI: {
       address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-      abi: "IERC20",
+      abi: "ERC20",
+      interfaceAbi: "IERC20",
     },
     WETH: {
       address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-      abi: "IERC20",
+      abi: "ERC20",
+      interfaceAbi: "IERC20",
     },
   },
 };
