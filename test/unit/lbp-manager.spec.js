d797ddd3ab93238dedaf1f13affc2e025f2b29ba
Marvin <marvin88gr@gmail.com>
2021-10-06 23:52:43 +0200

    Remove keyword only

--- test/unit/lbp-manager.spec.js
+++ test/unit/lbp-manager.spec.js
@@ -165,7 +165,7 @@ const setupInitialState = async (contractInstances, initialState) => {
   };
 };
 
-describe.only(">> Contract: LBPManager", () => {
+describe(">> Contract: LBPManager", () => {
   let poolId,
     admin,
     owner,
