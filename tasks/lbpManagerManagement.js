0c29a0fac121ee2712ae8afeeb676bbd5b33406a
Marvin <marvin88gr@gmail.com>
2021-10-13 10:57:16 +0200

    formating

--- tasks/lbpManagerManagement.js
+++ tasks/lbpManagerManagement.js
@@ -3,7 +3,6 @@ const { task } = require("hardhat/config");
 const { api } = require("./utils/gnosis.js");
 const { LBPManagerArguments } = require("../test/test-сonfig.json");
 
-
 task(
   "sendTransactionLBP",
   "send transaction for deploying LBPManager to Gnosis Safe"
@@ -17,7 +16,9 @@ task(
     const gnosis = api(safeAddress, network.name);
     const { root } = await ethers.getNamedSigners();
     const { deploy } = deployments;
-    const lbpManagerFactoryInstance = await ethers.getContract("LBPManagerFactory");
+    const lbpManagerFactoryInstance = await ethers.getContract(
+      "LBPManagerFactory"
+    );
     const signerV2Instance = await ethers.getContract("SignerV2");
     const startTime = Math.floor(Date.now() / 1000);
     const endTime = startTime + 1000;
@@ -35,7 +36,7 @@ task(
     transaction.to = lbpManagerFactoryInstance.address;
     transaction.value = 0;
     transaction.operation = 0;
-    
+
     const LBPManagerArgumentsArray = [
       LBPManagerArguments.ADMIN,
       LBPManagerArguments.BENEFICIARY,
@@ -47,55 +48,62 @@ task(
       [startTime, endTime],
       LBPManagerArguments.endWeights,
       LBPManagerArguments.fees,
-      LBPManagerArguments.metadata
-    ]
-    transaction.data = (await lbpManagerFactoryInstance.populateTransaction.deployLBPManager(...LBPManagerArgumentsArray)).data;
-    
+      LBPManagerArguments.metadata,
+    ];
+    transaction.data = (
+      await lbpManagerFactoryInstance.populateTransaction.deployLBPManager(
+        ...LBPManagerArgumentsArray
+      )
+    ).data;
+
     const estimate = await gnosis.getEstimate(transaction);
     transaction.safeTxGas = estimate.data.safeTxGas;
-    
+
     transaction.safe = safeAddress;
-    transaction.baseGas        = 0;
-    transaction.gasPrice       = 0;
-    transaction.gasToken       = '0x0000000000000000000000000000000000000000';
-    transaction.refundReceiver = '0x0000000000000000000000000000000000000000';
+    transaction.baseGas = 0;
+    transaction.gasPrice = 0;
+    transaction.gasToken = "0x0000000000000000000000000000000000000000";
+    transaction.refundReceiver = "0x0000000000000000000000000000000000000000";
     transaction.nonce = await gnosis.getCurrentNonce();
 
-    const {hash, signature} = await signerV2Instance.callStatic.generateSignature(
-			transaction.to,
-			transaction.value,
-			transaction.data,
-			transaction.operation,
-			transaction.safeTxGas,
-			transaction.baseGas,
-			transaction.gasPrice,
-			transaction.gasToken,
-			transaction.refundReceiver,
-			transaction.nonce
-		);
+    const { hash, signature } =
+      await signerV2Instance.callStatic.generateSignature(
+        transaction.to,
+        transaction.value,
+        transaction.data,
+        transaction.operation,
+        transaction.safeTxGas,
+        transaction.baseGas,
+        transaction.gasPrice,
+        transaction.gasToken,
+        transaction.refundReceiver,
+        transaction.nonce
+      );
     transaction.contractTransactionHash = hash;
     transaction.signature = signature;
 
     transaction.sender = signerV2Instance.address;
 
-    (await signerV2Instance.generateSignature(
-      transaction.to,
-      transaction.value,
-      transaction.data,
-      transaction.operation,
-      transaction.safeTxGas,
-      transaction.baseGas,
-      transaction.gasPrice,
-      transaction.gasToken,
-      transaction.refundReceiver,
-      transaction.nonce)).wait()
-      .then(
-          async () => await gnosis.sendTransaction(transaction)
-      );
+    (
+      await signerV2Instance.generateSignature(
+        transaction.to,
+        transaction.value,
+        transaction.data,
+        transaction.operation,
+        transaction.safeTxGas,
+        transaction.baseGas,
+        transaction.gasPrice,
+        transaction.gasToken,
+        transaction.refundReceiver,
+        transaction.nonce
+      )
+    )
+      .wait()
+      .then(async () => await gnosis.sendTransaction(transaction));
   });
 
-  // task("initializeLBP", "Initializes the LBP after task sentTransactionLBP has been accepted")
-  // .addParam("sender", "address of liquidity provider")
-  // .setAction(async ({sender: senderAddress}, {ethers}) => {
-  //   console.log("deploy")
-  // })
+// task("initializeLBP", "Initializes the LBP after task sentTransactionLBP has been accepted")
+// .addParam("sender", "address of liquidity provider")
+// .setAction(async ({sender: senderAddress}, {ethers}) => {
+//   console.log("deploy")
+// })
--- test/unit/lbp-manager.spec.js
+++ test/unit/lbp-manager.spec.js
@@ -176,7 +176,7 @@ const setupInitialState = async (contractInstances, initialState) => {
   };
 };
 
-describe(">> Contract: LBPManager", () => {
+describe.only(">> Contract: LBPManager", () => {
   let poolId,
     admin,
     owner,
@@ -226,6 +226,8 @@ describe(">> Contract: LBPManager", () => {
     tokenAddresses = tokenInstances.map((token) => token.address);
 
     fees = [SWAP_FEE_PERCENTAGE, FEE_PERCENTAGE_ZERO];
+    console.log(fees[0].toString());
+    console.log(fees[1].toString());
 
     initializeLBPManagerParams = paramGenerator.initializeParams(
       lbpFactoryInstance.address,
