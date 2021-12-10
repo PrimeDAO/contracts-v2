903b297df38d7f3d1df90cc54990c889ed5c7e71
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-12-10 14:59:58 +0530

    pre-commit lint fix

--- contracts/migration/Prime.sol
+++ contracts/migration/Prime.sol
@@ -21,6 +21,7 @@ import "openzeppelin-contracts-sol8/token/ERC20/extensions/ERC20Capped.sol";
  */
 contract Prime is ERC20Capped {
     uint256 supply = 100000000000000000000000000;
+
     constructor() public ERC20("Prime", "D2D") ERC20Capped(supply) {
         ERC20._mint(msg.sender, supply);
     }
