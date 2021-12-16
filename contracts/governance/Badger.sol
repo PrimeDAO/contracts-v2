8a2f85ac31a333da6e0e36cf065c0d09291e8df2
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-12-16 16:51:31 +0100

    moves struct inside the contract

--- contracts/governance/Badger.sol
+++ contracts/governance/Badger.sol
@@ -5,13 +5,17 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "../libraries/Strings.sol";
 
-struct TokenTier {
-    string uriId;
-    bool transferable;
-}
-
 contract Badger is Ownable, ERC1155 {
     using Strings for string;
+
+    /*
+        Structs
+    */
+
+    struct TokenTier {
+        string uriId;
+        bool transferable;
+    }
 
     /*
         State variables
