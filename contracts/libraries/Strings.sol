8efca7b44f5c6d67eebb6c4d258478e7311f62dc
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-12-16 14:53:24 +0100

    formats library function

--- contracts/libraries/Strings.sol
+++ contracts/libraries/Strings.sol
@@ -2,7 +2,11 @@
 pragma solidity ^0.8.0;
 
 library Strings {
-    function append(string memory a, string memory b) internal pure returns (string memory) {
+    function append(string memory a, string memory b)
+        internal
+        pure
+        returns (string memory)
+    {
         return string(abi.encodePacked(a, b));
     }
 }
