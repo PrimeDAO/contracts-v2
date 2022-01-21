19c9746c25ff175a68052030020367d8cdb014f0
Zitzak <marvin88gr@gmail.com>
2022-01-21 19:55:56 +0100

    Fix test

--- contracts/test/PrimeToken.sol
+++ contracts/test/PrimeToken.sol
@@ -11,11 +11,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // solium-disable linebreak-style
 
-pragma solidity 0.8.6;
+pragma solidity ^0.8.6;
 
-import "openzeppelin-contracts-sol8/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract Prime is ERC20Capped {
+contract PrimeToken is ERC20Capped {
     constructor(
         uint256 initialSupply,
         uint256 cap,
