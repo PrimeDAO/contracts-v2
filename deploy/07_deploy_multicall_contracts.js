51f69cb25cdf6ae89811108aa5364dc14a6a4a91
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2022-01-27 19:21:30 +0530

    added formatted multicall deployment script

--- deploy/07_deploy_multicall_contracts.js
+++ deploy/07_deploy_multicall_contracts.js
@@ -1,14 +1,13 @@
 const deployFunction = async ({ getNamedAccounts, deployments, network }) => {
-    const { deploy } = deployments;
-    const { root } = await getNamedAccounts();
-  
-    await deploy("Multicall", {
-      from: root,
-      args: [],
-      log: true,
-    });
-  };
-  
-  module.exports = deployFunction;
-  module.exports.tags = ["Multicall"];
-  
+  const { deploy } = deployments;
+  const { root } = await getNamedAccounts();
+
+  await deploy("Multicall", {
+    from: root,
+    args: [],
+    log: true,
+  });
+};
+
+module.exports = deployFunction;
+module.exports.tags = ["Multicall"];
