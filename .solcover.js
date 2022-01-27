4cfa234e6fd51dfe277750893c300849f8a2bbca
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2022-01-27 19:29:38 +0530

    added Multicall.sol to skipFiles List of Solidity Coverage

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,3 @@
 module.exports = {
-  skipFiles: ["test/CustomERC20Mock.sol","test/ERC20Mock.sol","test/Imports.sol","test/PrimeToken.sol"]
+  skipFiles: ["test/CustomERC20Mock.sol","test/ERC20Mock.sol","test/Imports.sol","test/PrimeToken.sol", "utils/Multicall.sol"]
 };
